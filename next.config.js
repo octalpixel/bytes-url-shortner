165b25a107a8d4add907bd935398d571fc09ca37
octalpixel <mithushan>
2021-11-16 01:20:33 +0530

    change schema

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /**
  * @link https://nextjs.org/docs/api-reference/next.config.js/introduction
  */
-module.exports = {};
+module.exports = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.resolve.alias['@prisma/client$'] = require.resolve('@prisma/client');
+        return config;
+    },
+};
--- src/pages/_middleware.tsx
+++ src/pages/_middleware.tsx
@@ -1,8 +1,15 @@
-import type { NextRequest } from 'next/server';
 import { PrismaClient } from '@prisma/client';
+import { nanoid } from 'nanoid';
+import type { NextRequest } from 'next/server';
 
 const prisma = new PrismaClient();
 
-export async function  middleware(req: NextRequest) {
-//   const links = await prisma.f
+export async function middleware(req: NextRequest) {
+  const links = await prisma.link.findMany({});
+
+  return new Response(JSON.stringify(links), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 }
